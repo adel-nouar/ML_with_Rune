c81d39339e9bea7953fbe2f0036b98e25b8376f9
Adel NOUAR <adel.nouar@protonmail.com>
2021-11-16 18:55:21 +0100

    10: Taking files from Rune's github and creating clear_cells program

---
+++ clear_cells.py
@@ -0,0 +1,30 @@
+'''
+Take a jupyter notebook file, and clear all the code cells.
+Use:
+python clear_cells.py <file.ipynb>
+
+if you specify a second arguments, the source file will not change
+but by default, the source file will be cleared from code cells.
+'''
+
+# TODO: Managing exceptions, cleaning code.
+
+import sys
+import nbformat as nbf
+
+nb_args = len(sys.argv)
+if nb_args >1 and sys.argv[1][-6] == '.ipynb':
+    src_file = sys.argv[1]
+    ntbk = nbf.read(src_file, nbf.NO_CONVERT)
+    new_ntbk = ntbk
+    new_ntbk.cells = [cell if cell.cell_type == "markdown" else nbf.v4.new_code_cell() for cell in ntbk.cells ]
+    if nb_args > 2:
+        output_file = ''
+        if sys.argv[2][-6] == '.ipynb':
+            output_file = sys.argv[2]
+        else:
+            output_file = sys.argv[2] + ".ipynb"
+        nbf.write(new_ntbk, output_file, version=nbf.NO_CONVERT)
+    else:
+        nbf.write(new_ntbk, src_file, version=nbf.NO_CONVERT)
+
